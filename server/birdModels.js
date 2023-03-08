cecd64beb26ebb3734519d3b9eadc8952f3053cc
Victoria Dillman <victoriadillman@Victorias-MacBook-Pro.local>
2023-03-07 20:00:51 -0500

    barebones structure

---
+++ server/birdModels.js
@@ -0,0 +1,15 @@
+// Setting up my MongoDB Database for adding birds to profile later
+const mongoose = require('mongoose');
+// I don't feel confident about this link.. I pulled it from my created database
+const MONGO_URI = 'mongodb+srv://victoria:e7wJkwTxgJCHjh86@cluster1.cmxjvoo.mongodb.net/test'
+
+mongoose.connect(MONGO_URI, {
+  // options for the connect method to parse the URI
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // sets the name of the DB that our collections are part of
+  dbName: 'birds'
+})
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch(err => console.log(err));
+
